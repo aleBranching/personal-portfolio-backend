853ed839b65f0c6f149601f6b5c6169eb383be75
aleBranching <aleksas773@googlemail.com>
2023-08-02 19:31:48 +0100

    final build

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { default: axios } = require("axios");
+const { toHTML, toMarkdownV2 } = require("@telegraf/entity");
+const cors = require("cors");
 
 let { BOT_TOKEN, SERVER_URL, MY_CHATID, WEBHOOK_TOKEN } = process.env;
 // Of the form of
@@ -15,12 +17,13 @@ const WEBHOOK_URL = SERVER_URL + URI;
 
 const MY_URI = "/" + URI;
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  console.log(req.url);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.url);
+//   next();
+// });
 
 const init = async () => {
   const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
@@ -29,17 +32,35 @@ const init = async () => {
   console.log("the res.data", res.data);
 };
 
-app.post(MY_URI, async (req, res) => {
+// used to find the initial chat id
+
+// app.post(MY_URI, async (req, res) => {
+//   console.log("here");
+//   console.log(req.body);
+//   await axios.post(`${TELEGRAM_API}/sendMessage`, {
+//     chat_id: MY_CHATID,
+//     text: "dummmy text",
+//   });
+//   return res.send();
+// });
+app.post("/", async (req, res) => {
   console.log("here");
-  console.log(req.body);
+  let email = toMarkdownV2({ text: req.body.email, entities: [] });
+  let message = toMarkdownV2({ text: req.body.message, entities: [] });
+  var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+  console.log(email);
+  console.log(message);
+  console.log("ip address:", ip);
+
+  ip = toMarkdownV2({ text: ip, entities: [] });
+
   await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    parse_mode: "MarkdownV2",
     chat_id: MY_CHATID,
-    text: "dummmy text",
+    text: `*email*: ${email} \n*IP address*:${ip}\n*message:*\n${message}`,
+    //  text: "*gal*"
   });
   return res.send();
-});
-app.post("/", async (req, res) => {
-  console.log(req.body);
 });
 
 app.listen(process.env.PORT || 5000, async () => {
